d09bca7c299e7d273aa73e76fcbd9f0c8bdbcbb7
Casso Rabello <0cassiorabello@gmail.com>
2025-09-07 17:49:52 -0300

    Botão de listagem de produtos funcionando corretamente

--- LeiloesTDSat/src/cadastroVIEW.java
+++ LeiloesTDSat/src/cadastroVIEW.java
@@ -200,7 +200,7 @@ public class cadastroVIEW extends javax.swing.JFrame {
 
     private void btnProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProdutosActionPerformed
         listagemVIEW listagem = new listagemVIEW(); 
-        listagem.setVisible(true);
+        listagem.setVisible(true); //Listagem de produtos funcionando
     }//GEN-LAST:event_btnProdutosActionPerformed
 
     /**
